1a0ce0660351100a7c70831dcf93482504223f62
Oreoluwa Lawal <oreolawal@gmail.com>
2023-03-07 17:22:12 +0000

    linux-app

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -4,19 +4,19 @@
 
 --create tables
 CREATE TABLE IF NOT EXISTS PUBLIC.host_info
-                            (
-                                id               SERIAL NOT NULL,
-                                hostname         VARCHAR NOT NULL,
-                                cpu_number       INT2 NOT NULL,
-                                cpu_architecture VARCHAR NOT NULL,
-                                cpu_model        VARCHAR NOT NULL,
-                                cpu_mhz          FLOAT8 NOT NULL,
-                                l2_cache         INT4 NOT NULL,
-                                "timestamp"      TIMESTAMP NULL,
-                                total_mem        INT4 NULL,
-                                CONSTRAINT host_info_pk PRIMARY KEY (id),
-                                CONSTRAINT host_info_un UNIQUE (hostname)
-                             );
+  (
+     id               SERIAL NOT NULL,
+     hostname         VARCHAR NOT NULL,
+     cpu_number       INT2 NOT NULL,
+     cpu_architecture VARCHAR NOT NULL,
+     cpu_model        VARCHAR NOT NULL,
+     cpu_mhz          FLOAT8 NOT NULL,
+     l2_cache         INT4 NOT NULL,
+     "timestamp"      TIMESTAMP NULL,
+     total_mem        INT4 NULL,
+     CONSTRAINT host_info_pk PRIMARY KEY (id),
+     CONSTRAINT host_info_un UNIQUE (hostname)
+  );
 
 
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
